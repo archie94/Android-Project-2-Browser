ba4b1db8148cc56904760e5ea06969018aeb1887
Arka Prava Basu <arkaprava94@gmail.com>
2015-08-12 22:37:43 +0530

    Minor modifications-app restricted to portrait mode, address bar selected on focus

--- gen/com/example/zsurfer/R.java
+++ gen/com/example/zsurfer/R.java
@@ -21,8 +21,13 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int custom_button_go=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int back=0x7f020000;
+        public static final int custom_button_back=0x7f020001;
+        public static final int custom_button_go=0x7f020002;
+        public static final int forward=0x7f020003;
+        public static final int home=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int refresh=0x7f020006;
     }
     public static final class id {
         public static final int WebView=0x7f080000;
