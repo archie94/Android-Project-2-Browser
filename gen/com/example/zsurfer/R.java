4789342fb1b15bd7abdc563eac3228b8adb4c138
Arka Prava Basu <arkaprava94@gmail.com>
2015-09-08 01:49:03 +0530

    Added layout for bookmarks

--- gen/com/example/zsurfer/R.java
+++ gen/com/example/zsurfer/R.java
@@ -46,7 +46,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_simple_browser=0x7f030000;
-        public static final int history=0x7f030001;
+        public static final int bookmarks=0x7f030001;
+        public static final int history=0x7f030002;
     }
     public static final class menu {
         public static final int simple_browser=0x7f070000;
