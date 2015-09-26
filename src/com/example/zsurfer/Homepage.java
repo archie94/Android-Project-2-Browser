9bfd983063485b7733c426eadc7931aa6f124300
Arka Prava Basu <arkaprava94@gmail.com>
2015-09-26 17:29:03 +0530

    Added a BaseAdapter class to fill in GridView

--- src/com/example/zsurfer/Homepage.java
+++ src/com/example/zsurfer/Homepage.java
@@ -1,7 +1,12 @@
 package com.example.zsurfer;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 
 public class Homepage extends Activity
@@ -18,5 +23,59 @@ public class Homepage extends Activity
 		
 	}
 
+	
+}
+
+class WebPages
+{
+	private String wbpg;
+	WebPages(String wbpg)
+	{
+		this.wbpg=wbpg;
+	}
+}
+
+
+class HomeAdapter extends BaseAdapter
+{
+
+	ArrayList<WebPages> list;
+	HomeAdapter()
+	{
+		list=new ArrayList<WebPages>();
+		String pages[]={"Gmail","Google","Facebook","Twitter","Youtube","Quora","Flipkart","AmazonIndia","Wikipedia","Yahoo"};
+		for(int i=0;i<pages.length;i++)
+		{
+			WebPages temp=new WebPages(pages[i]);
+			list.add(temp);
+		}
+	}
+	@Override
+	public int getCount() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
