4263208f13bcdc732b158c4efc4e4661dda369d3
Arka Prava Basu <arkaprava94@gmail.com>
2015-10-02 22:01:36 +0530

    Changes in homepage.xml

--- gen/com/example/zsurfer/R.java
+++ gen/com/example/zsurfer/R.java
@@ -41,7 +41,7 @@ public final class R {
     }
     public static final class id {
         public static final int WebView=0x7f080003;
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f080013;
         public static final int bBKMRK=0x7f08000a;
         public static final int bBack=0x7f080004;
         public static final int bForward=0x7f080005;
@@ -53,7 +53,9 @@ public final class R {
         public static final int bkmk_dialog=0x7f08000d;
         public static final int editText1=0x7f080008;
         public static final int gridView_homepage=0x7f08000f;
-        public static final int grid_imageView=0x7f080010;
+        public static final int grid_imageView=0x7f080012;
+        public static final int home_bGO=0x7f080011;
+        public static final int home_editText1=0x7f080010;
         public static final int listViewBookmarks=0x7f08000c;
         public static final int listViewHistory=0x7f08000e;
         public static final int mainLayout=0x7f080000;
